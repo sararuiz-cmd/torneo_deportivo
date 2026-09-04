49cd7d57155409da7cf8ad3d16a9fda3bfd01c2b
Sara Ruiz <svruiz@uamv.edu.ni>
2026-09-04 17:37:16 -0600

    feat: TableView Controller

--- src/main/java/ni/edu/uam/torneo_deportivo/controllers/TableviewController.java
+++ src/main/java/ni/edu/uam/torneo_deportivo/controllers/TableviewController.java
@@ -1,8 +1,17 @@
 package ni.edu.uam.torneo_deportivo.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class TableviewController {
     @FXML
+    private TextField NombreCompleto;
+    @FXML
+    private TextField Edad;
+    @FXML
+    private TextField telefono;
+    @FXML
+
 
 }
